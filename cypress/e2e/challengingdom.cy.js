4a30af2b878527b63752c9a25cef992b06d0308f
rgybrgyb <rgybrgybrgyb@gmail.com>
2024-05-16 10:48:45 +0100

    added challenging DOM first draft

---
+++ cypress/e2e/challengingdom.cy.js
@@ -0,0 +1,8 @@
+describe('Challenging DOM test', () => {
+    beforeEach(() => {
+        cy.visit('/challenging_dom/')
+    })
+    it('Locates and clicks the top button on the page', () => {
+        cy.get
+    })
+})
