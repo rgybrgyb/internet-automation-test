48eec219efe64d6d8e1c4a9a412038de62bf094a
rgybrgyb <rgybrgybrgyb@gmail.com>
2024-05-20 19:10:11 +0100

    changed contextmenu description

--- cypress/e2e/contextmenu.cy.js
+++ cypress/e2e/contextmenu.cy.js
@@ -2,7 +2,7 @@ describe('Context Menu Test', () => {
     beforeEach(() => {
         cy.visit('/context_menu')
     })
-    it('Right clicks in the box and clicks ok on the resulting context menu', () => {
+    it('Right clicks in the box and clicks ok on the resulting context menu and checks the alert text matches expectations', () => {
         const alertShown = cy.stub().as("alertShown")
         cy.on('window:alert', alertShown)
         cy.get('[id="hot-spot"]').rightclick()
