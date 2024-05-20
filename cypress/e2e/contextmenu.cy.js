449ce93681921cfc4d75b20f2f7925d9a04df3a4
rgybrgyb <rgybrgybrgyb@gmail.com>
2024-05-20 19:07:43 +0100

    Added contextmenu test

---
+++ cypress/e2e/contextmenu.cy.js
@@ -0,0 +1,11 @@
+describe('Context Menu Test', () => {
+    beforeEach(() => {
+        cy.visit('/context_menu')
+    })
+    it('Right clicks in the box and clicks ok on the resulting context menu', () => {
+        const alertShown = cy.stub().as("alertShown")
+        cy.on('window:alert', alertShown)
+        cy.get('[id="hot-spot"]').rightclick()
+        cy.get("@alertShown").should("have.been.calledOnceWith", "You selected a context menu")
+    })
+})
