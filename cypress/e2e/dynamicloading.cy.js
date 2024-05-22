3ccb816ced5c0197a1cb8ce59636341abf5d719d
rgybrgyb <rgybrgybrgyb@gmail.com>
2024-05-23 00:47:46 +0100

    Added dynamicloading

---
+++ cypress/e2e/dynamicloading.cy.js
@@ -0,0 +1,16 @@
+describe('Dynamic Controls test', () => {
+    it('Checks that there is no text, clicks the button, then checks that there is text on page 1', () => {
+        cy.visit('/dynamic_loading/1')
+        cy.get('#finish > h4').should('not.be.visible')
+        cy.get('button').click()
+        cy.wait(6000)
+        cy.get('#finish > h4').should('be.visible')
+    })
+    it('Checks that there is no text, clicks the button, then checks that there is text on page 2', () => {
+        cy.visit('/dynamic_loading/2')
+        cy.get('#finish > h4').should('not.exist')
+        cy.get('button').click()
+        cy.wait(6000)
+        cy.get('#finish > h4').contains('Hello')
+    })
+})
